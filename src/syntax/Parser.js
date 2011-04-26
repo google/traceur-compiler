1f51a4689dd23a2ff4f114f43ec5aabce0bcc40a
arv@google.com <arv@google.com@27ec44a0-f51e-dfec-681c-368ff20b0e0a>
2011-04-26 22:21:26 +0000

    Treat END_OF_FILE as implicit semi colon.
    This fixes the issue where one always have to add a new line at the end of a
    file.
    
    BUG=
    TEST=
    
    Review URL: https://chromereviews.googleplex.com/2812013
    
    git-svn-id: https://traceur-compiler.googlecode.com/svn/trunk@166 27ec44a0-f51e-dfec-681c-368ff20b0e0a

--- src/syntax/Parser.js
+++ src/syntax/Parser.js
@@ -3161,7 +3161,10 @@ traceur.define('syntax', function() {
      * @private
      */
     peekImplicitSemiColon_: function() {
-      return this.getNextLine_() > this.getLastLine_() || this.peek_(TokenType.SEMI_COLON) || this.peek_(TokenType.CLOSE_CURLY);
+      return this.getNextLine_() > this.getLastLine_() ||
+          this.peek_(TokenType.SEMI_COLON) ||
+          this.peek_(TokenType.CLOSE_CURLY) ||
+          this.peek_(TokenType.END_OF_FILE);
     },
 
     /**
