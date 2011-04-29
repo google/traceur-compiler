0490141632efef96ca7e2147afa2a094024e685d
rnystrom@google.com <rnystrom@google.com@27ec44a0-f51e-dfec-681c-368ff20b0e0a>
2011-04-29 18:56:53 +0000

    Script to run feature tests from node.
    The code is a bit more hackish than I'd like, but works. Many tests
    currently fail in node. We apparently generate code with trailing
    commas in argument lists, which doesn't make node happy. The
    browsers let us slide.
    
    BUG=
    TEST=
    
    Review URL: https://chromereviews.googleplex.com/2802035
    
    git-svn-id: https://traceur-compiler.googlecode.com/svn/trunk@176 27ec44a0-f51e-dfec-681c-368ff20b0e0a

---
+++ test/testfeatures.js
@@ -0,0 +1,196 @@
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Reads a script and eval's it into the global scope.
+ * TODO: this is needed for now because of how our scripts are designed.
+ * Change this once we have a module system.
+ * @param {string} filename
+ */
+function importScript(filename) {
+  // TODO(rnystrom): Hack. Assumes this is being run from a sibling of src/.
+  filename = path.join('../src/', filename);
+  var script = fs.readFileSync(filename, 'utf8');
+  if (!script) {
+    throw new Error('Failed to import ' + filename);
+  }
+  eval.call(global, script);
+}
+
+/**
+ * Show a failure message for the given script.
+ */
+function failScript(script, message) {
+  console.log('\x1B[31mFAIL\x1B[0m ' + script);
+  console.log('     ' + message);
+}
+
+/**
+ * Load, compile, and execute the feature script at the given path.
+ */
+function testScript(filePath) {
+  var script = fs.readFileSync(filePath, 'utf8');
+  if (!script) {
+    failScript(filePath, 'Could not read file.');
+    return false;
+  }
+
+  var reporter = new traceur.util.ErrorReporter();
+  var sourceFile = new traceur.syntax.SourceFile(filePath, script);
+  var tree = traceur.codegeneration.Compiler.compileFile(reporter, sourceFile);
+  
+  if (reporter.hadError()) {
+    failScript(filePath, 'Unexpected compile error in script.');
+    return false;
+  }
+
+  var javascript = traceur.codegeneration.ParseTreeWriter.write(tree, false);
+
+  try {
+    testScriptInContext(javascript);
+    return true;
+  } catch (e) {
+    if (e instanceof UnitTestError) {
+      failScript(filePath, e.message);
+    } else if (e instanceof SyntaxError) {
+      failScript(filePath, 'Compiled to invalid Javascript. Source:\n\n     ' +
+          javascript.trim().replace(/\n/g, '\n     ') + '\n\n' +
+          '     ' + e.toString());
+    } else {
+      failScript(filePath, 'Unexpected exception:\n' + e.toString());
+    }
+  }
+
+  return false;
+}
+
+function UnitTestError(message) {
+  this.message = message;
+}
+
+/**
+ * Feature scripts are evaluated in the context of this function, so it contains
+ * the functions that the scripts need access to.
+ */
+function testScriptInContext(javascript) {
+  // Define this so that scripts that use the DOM know not to run.
+  var IN_BROWSER = false;
+
+  // Define some rudimentary versions of the JSUnit assertions that the
+  // feature tests use.
+  function fail(message) {
+    throw new UnitTestError(message);
+  }
+
+  function assertEquals(expected, actual) {
+    if (actual !== expected) {
+      fail('Expected ' + expected + ' but was ' + actual + '.');
+    }
+  }
+
+  function assertNotEquals(expected, actual) {
+    if (actual === expected) {
+      fail('Expected ' + expected + ' to not be ' + actual + '.');
+    }
+  }
+
+  function assertNotNull(actual) {
+    if (actual === null) {
+      fail('Unexpected null.');
+    }
+  }
+
+  function assertFalse(actual) {
+    assertEquals(false, actual);
+  }
+
+  function assertTrue(actual) {
+    assertEquals(true, actual);
+  }
+
+  function assertUndefined(actual) {
+    assertEquals(undefined, actual);
+  }
+
+  function assertThrows(fn) {
+    try {
+      fn();
+      fail('Function should have thrown and did not.');
+    } catch (e) {
+      // Do nothing.
+    }
+  }
+
+  function assertNoOwnProperties(o) {
+    var m = Object.getOwnPropertyNames(o);
+    if (m.length) {
+      fail('Unexpected members found:' + m.join(', '));
+    }
+  }
+
+  function assertHasOwnProperty(o) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0; i < args.length; i ++) {
+      var m = args[i];
+      if (!o.hasOwnProperty(m)) {
+        fail('Expected member ' + m + ' not found.');
+      }
+    }
+  }
+
+  function assertLacksOwnProperty(o) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0; i < args.length; i ++) {
+      var m = args[i];
+      if (o.hasOwnProperty(m)) {
+        fail('Unxpected member ' + m + ' found.');
+      }
+    }
+  }
+
+  // TODO(rnystrom): Hack. Don't let Traceur spew all over our beautiful
+  // test results.
+  var console = {
+    log: function() {},
+    info: function() {},
+    error: function() {}
+  };
+  
+  eval(javascript);
+}
+
+/**
+ * Recursively walk the "feature" directory and run each feature script found.
+ */
+function runFeatureScripts(dir) {
+  var contents = fs.readdirSync(dir);
+  for (var i = 0; i < contents.length; i++) {
+    var filePath = path.join(dir, contents[i]);
+    var stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      runFeatureScripts(filePath);
+    } else if (path.extname(filePath) == '.js') {
+      tests++;
+      if (testScript(filePath)) passes++;
+    }
+  }
+}
+
+// Allow traceur.js to use importScript.
+global.importScript = importScript;
+
+// Load the compiler.
+importScript('traceur.js');
+
+// Run all of the feature scripts.
+var tests  = 0;
+var passes = 0;
+runFeatureScripts('feature');
+
+if (passes == tests) {
+  console.log('Passed all \x1B[32m' + tests + '\x1B[0m tests.');
+} else {
+  console.log();
+  console.log('Passed \x1B[32m' + passes + '\x1B[0m and failed \x1B[31m' +
+      (tests - passes) + '\x1B[0m out of ' + tests + ' tests.');
+}
