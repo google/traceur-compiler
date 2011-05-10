415a858d32c7cb29bd81bebc2a2d22ff3b088030
arv@google.com <arv@google.com@27ec44a0-f51e-dfec-681c-368ff20b0e0a>
2011-05-10 18:43:00 +0000

    Fix invalid test and disable it due to V8 bug
    git-svn-id: https://traceur-compiler.googlecode.com/svn/trunk@289 27ec44a0-f51e-dfec-681c-368ff20b0e0a

--- test/feature/Classes/NameBinding.js
+++ test/feature/Classes/NameBinding.js
@@ -11,7 +11,7 @@ class ElementHolder {
   }
 
   makeFilterLostThis() {
-    return function (x) { return x == this.element; }
+    return function () { return this; }
   }
 
   makeFilterHidden(element) {
@@ -26,7 +26,9 @@ var obj = new ElementHolder();
 obj.element = 40;
 assertEquals(40, obj.getElement());
 assertTrue(obj.makeFilterCapturedThis()(40));
-assertFalse(obj.makeFilterLostThis()(40));
+
+// http://code.google.com/p/v8/issues/detail?id=1381
+// assertUndefined(obj.makeFilterLostThis()());
 
 obj.element = 39;
 assertFalse(obj.makeFilterCapturedThis()(40));
