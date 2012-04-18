02e94e904180e354e629cf58d85d3bd0f28ad336
Erik Arvidsson <arv@chromium.org>
2012-04-17 21:51:27 -0700

    Extract ErrorReporter.format from repl.html
    BUG=None
    TEST=error_reporter_test.html
    
    Review URL: https://codereview.appspot.com/6057043

--- src/util/ErrorReporter.js
+++ src/util/ErrorReporter.js
@@ -64,6 +64,33 @@ traceur.define('util', function() {
     }
   };
 
+  /**
+   * Formats an error message.
+   * @param  {SourcePosition} location If present the source position is
+   *     prepended to the message. Use |null| to not include the source
+   *     position.
+   * @param  {string} text The text to output. %s and %% are the only supported
+   *     place holders.
+   * @param  {Array} opt_args Array values to populate the placeholders with.
+   * @return {string} The text where the source position has been prepended and
+   *     where the place holders have been replaced.
+   */
+  ErrorReporter.format = function(location, text, opt_args) {
+    var i = 0;
+    text = text.replace(/%./g, function(s) {
+      switch (s) {
+        case '%s':
+          return opt_args && opt_args[i++];
+        case '%%':
+          return '%';
+      }
+      return s;
+    });
+    if (location)
+      text = location + ': ' + text;
+    return text;
+  };
+
   return {
     ErrorReporter: ErrorReporter
   };
