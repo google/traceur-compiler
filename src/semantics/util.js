a4c7d0ca11923dd2c070162a334e58d75b9df1a8
Erik Arvidsson <arv@chromium.org>
2012-10-19 10:54:01 -0400

    Add src/semantics/util.js which I forgot to add in the last commit.

---
+++ src/semantics/util.js
@@ -0,0 +1,44 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ParseTreeType from '../syntax/trees/ParseTree.js';
+import TokenType from '../syntax/TokenType.js';
+
+/**
+ * Returns the string value for a literal token.
+ * @param {LiteralToken} token
+ * @return {string}
+ */
+export function evaluateStringLiteral(token) {
+  // TODO(arv): I feel dirty using eval here. We should just do the right
+  // thing!
+  return eval(token.value);
+}
+
+/**
+ * @param {Array.<ParseTree>} list
+ * @return {boolean}
+ */
+export function hasUseStrict(list) {
+  var li;
+  if (!list || !list.length || !(li = list[0]))
+    return false;
+  if (li.type !== ParseTreeType.EXPRESSION_STATEMENT || !(li = li.expression))
+    return false;
+  if (li.type !== ParseTreeType.LITERAL_EXPRESSION   || !(li = li.literalToken))
+    return false;
+  if (li.type !== TokenType.STRING)
+    return false;
+  return evaluateStringLiteral(li) === 'use strict';
+}
