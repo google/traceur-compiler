ec405eeae9619e5bd2e4d86f39f9613d3d755589
Erik Arvidsson <arv@chromium.org>
2013-02-24 15:10:50 -0500

    Update the copyrights to say "Tracuer Authors"
    Add Google and Chromium to the AUTHORS file.
    
    TBR=slightlyoff
    
    Review URL: https://codereview.appspot.com/7402053

--- src/node/file-util.js
+++ src/node/file-util.js
@@ -1,4 +1,4 @@
-// Copyright 2013 Google Inc.
+// Copyright 2013 Traceur Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
