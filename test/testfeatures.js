google/traceur-compiler

commit 475b37c1df14a5f894e1c8011dd8d5e34c3cd520

Use a generated test list for node and html tests.
- Add a 'test-list' build target to generate the list.
- Add a dirwalk option to testfeatures.js to allow the option to
  emulate the previous directory walking behavior.

BUG=None
TEST=None

Review URL: https://codereview.appspot.com/7378058
Patch from Ben Chan <usrbincc@yahoo.com>.

test/testfeatures.js
@@ -17,6 +17,7 @@
 var fs = require('fs');
 var path = require('path');
 var testUtil = require('./test-utils.js');
+var testList;
 
 /**
  * Show a failure message for the given script.
@@ -245,24 +246,9 @@ function runFeatureScripts(dir) {
     if (stat.isDirectory()) {
       runFeatureScripts(filePath);
     } else if (path.extname(filePath) == '.js') {
-      clearLastLine();
-      if (passes === tests) {
-        print('Passed ' + green(passes) + ' so far. Testing: ' + filePath);
-      } else {
-        print('Passed ' + green(passes) + ' and failed ' +
-              red(tests - passes) + ' Testing: ' + filePath);
-      }
-      print('\n');
-
       if (errslast && errslast.indexOf(filePath) >= 0)
         continue;
-
-      tests++;
-      if (testScript(filePath))
-        passes++;
-
-      if (tests - passes > errsnew.length)
-        errsnew.push(filePath);
+      updateProgress(testScript, filePath);
     }
   }
 }
@@ -283,9 +269,34 @@ require('../src/node/traceur.js');
 
 print('\n');
 
-// Run all of the feature scripts.
+// Running counts of total and passed tests.
 var tests  = 0;
 var passes = 0;
+
+/**
+ * Run |testFunction| on |filePath| and update |tests| and |passes|
+ * appropriately, while also printing progress.
+ * @param {function} testFunction
+ * @param {string} filePath
+ */
+function updateProgress(testFunction, filePath) {
+  clearLastLine();
+  if (passes === tests) {
+    print('Passed ' + green(passes) + ' so far. Testing: ' + filePath);
+  } else {
+    print('Passed ' + green(passes) + ' and failed ' +
+          red(tests - passes) + ' Testing: ' + filePath);
+  }
+  print('\n');
+
+  tests++;
+
+  if (testScript(filePath))
+    passes++;
+
+  if (tests - passes > errsnew.length)
+    errsnew.push(filePath);
+}
 
 // errsfile is an optional argument that activates the following behavior:
 //
@@ -312,6 +323,7 @@ try {
 flags.setMaxListeners(100);
 flags.option('--errsfile <FILE>', 'path to the error file');
 flags.option('--failfast', 'exit if anything from the error file failed');
+flags.option('--dirwalk <DIR>', 'run all .js test files in <DIR>');
 flags.parse(process.argv);
 
 var errsfile = flags.errsfile;
@@ -322,19 +334,38 @@ if (errsfile && fs.existsSync(errsfile)) {
   print('Using error file \'' + errsfile + '\' ...\n\n');
   errslast = JSON.parse(fs.readFileSync(errsfile, 'utf8'));
   errslast.forEach(function(f) {
-    tests++;
-    if (testScript(f))
-      passes++;
-
-    if (tests - passes > errsnew.length)
-      errsnew.push(f);
+    try {
+      updateProgress(testScript, f);
+    } catch(e) {
+      failScript(String(e));
+      // Don't count this test in the total if the error was
+      // "ENOENT, no such file or directory".
+      if (e.code === 'ENOENT') {
+        tests--;
+      }
+    }
   });
-} else {
-  print('\n');
 }
 
-if (!flags.failfast || passes == tests)
-  runFeatureScripts(path.join(__dirname, 'feature'), errsnew);
+if (!flags.failfast || passes == tests) {
+  if (flags.dirwalk) {
+    runFeatureScripts(flags.dirwalk);
+  } else {
+    try {
+      testList = require('./test-list.js').testList;
+      testList.forEach(function(f) {
+        if (errslast && errslast.indexOf(filePath) >= 0)
+          return;
+        updateProgress(testScript, path.join(__dirname, 'feature', f));
+      });
+    } catch(e) {
+      if (!errslast) {
+        console.error(String(e));
+        process.exit(1);
+      }
+    }
+  }
+}
 
 clearLastLine();
 if (passes == tests) {

.gitignore
@@ -13,3 +13,4 @@ src/syntax/trees/ParseTreeType.js
 src/syntax/trees/ParseTrees.js
 test/errsfile.json
 test/bench/esprima
+test/test-list.js

Makefile
@@ -20,8 +20,13 @@ min: bin/traceur.min.js
 #   npm install uglify-js -g
 ugly: bin/traceur.ugly.js
 
-test: build
+test: build test/test-list.js
 	node test/testfeatures.js --errsfile test/errsfile.json
+
+test-list: test/test-list.js
+
+test/test-list.js: build/build-test-list.js
+	git ls-files test/feature | node $? > $@
 
 boot: clean build
 
@@ -61,7 +66,7 @@ src/syntax/trees/ParseTreeType.js: src/syntax/trees/trees.json build/build-parse
 bin/traceur.ugly.js: bin/traceur.js
 	uglifyjs bin/traceur.js --compress dead_code=true,unused=true,sequences=true,join_vars=true,evaluate=true,booleans=true,conditionals=true -m -o $@
 
-.PHONY: build min test force boot clean distclean
+.PHONY: build min test test-list force boot clean distclean
 
 -include build/dep.mk
 -include build/local.mk

build/build-test-list.js
@@ -1,0 +1,49 @@
+// Copyright 2013 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var fs = require('fs');
+
+var STDIN_FILENO = 0;
+
+/**
+ * Returns all input read from |fd| as a string, correctly handling ending of
+ * piped input.
+ * @param {number} fd File descriptor to read from.
+ * @return {string}
+ */
+function readSyncString(fd) {
+  var BUFFER_SIZE = 4096;
+
+  var buf = new Buffer(BUFFER_SIZE);
+  var len = 0;
+  var text = '';
+
+  while (len = fs.readSync(fd, buf, 0, BUFFER_SIZE, null)) {
+    text += buf.toString('utf8', 0, len);
+  }
+  return text;
+}
+
+var lines = readSyncString(STDIN_FILENO).split('\n').filter(function(x) {
+  // JS files only
+  return x && x.match('\\.js$');
+}).map(function(x) {
+  // remove "test/feature/"
+  return x.split('/').slice(2).join('/');
+});
+
+console.log('var testList = %s;', JSON.stringify(lines, null, 2));
+console.log('if (typeof exports === \'object\') exports.testList = testList;');

commit 5ef92faf758e95ae232a4429f65f534b09ec6a12

Make most path-handling relative to 'test/feature' to fix errsfile bug.
The main test run wasn't excluding already-run tests because it was
comparing absolute paths (test/errsfile.json) with paths relative to
'test/feature' (test/test-list.json).

This fixes (yet) another bug introduced in commit 475b37c1df14a5f8

BUG=None
TEST=None

Review URL: https://codereview.appspot.com/7563047
Patch from Ben Chan <usrbincc@yahoo.com>.

test/testfeatures.js
@@ -236,19 +236,22 @@ function clearLastLine() {
 }
 
 /**
- * Recursively walk the "feature" directory and run each feature script found.
+ * Recursively walk the directory |dir| and run each test script found.
+ * @param {string} dir Directory to walk. Absolute or relative to process.cwd()
+ * @param {string|undefined} basePath Base path for errslast and updateProgress.
  */
-function runFeatureScripts(dir) {
+function runTestScripts(dir, basePath) {
   var contents = fs.readdirSync(dir);
   for (var i = 0; i < contents.length; i++) {
     var filePath = path.join(dir, contents[i]);
     var stat = fs.statSync(filePath);
     if (stat.isDirectory()) {
-      runFeatureScripts(filePath);
+      runTestScripts(filePath, basePath);
     } else if (path.extname(filePath) == '.js') {
+      filePath = basePath ? path.relative(basePath, filePath) : filePath;
       if (errslast && errslast.indexOf(filePath) >= 0)
         continue;
-      updateProgress(testScript, filePath);
+      updateProgress(testScript, filePath, basePath);
     }
   }
 }
@@ -278,8 +281,9 @@ var passes = 0;
  * appropriately, while also printing progress.
  * @param {function} testFunction
  * @param {string} filePath
+ * @param {string|undefined} basePath Optional base path for |filePath|
  */
-function updateProgress(testFunction, filePath) {
+function updateProgress(testFunction, filePath, basePath) {
   clearLastLine();
   if (passes === tests) {
     print('Passed ' + green(passes) + ' so far. Testing: ' + filePath);
@@ -291,7 +295,8 @@ function updateProgress(testFunction, filePath) {
 
   tests++;
 
-  if (testScript(filePath))
+  var filePathFull = basePath ? path.join(basePath, filePath) : filePath;
+  if (testScript(filePathFull))
     passes++;
 
   if (tests - passes > errsnew.length)
@@ -330,12 +335,14 @@ var errsfile = flags.errsfile;
 var errsnew = [];
 var errslast;
 
+var basePath = path.join(__dirname, 'feature');
+
 if (errsfile && fs.existsSync(errsfile)) {
   print('Using error file \'' + errsfile + '\' ...\n\n');
   errslast = JSON.parse(fs.readFileSync(errsfile, 'utf8'));
   errslast.forEach(function(f) {
     try {
-      updateProgress(testScript, f);
+      updateProgress(testScript, f, basePath);
     } catch(e) {
       failScript(String(e));
       // Don't count this test in the total if the error was
@@ -349,14 +356,14 @@ if (errsfile && fs.existsSync(errsfile)) {
 
 if (!flags.failfast || passes == tests) {
   if (flags.dirwalk) {
-    runFeatureScripts(flags.dirwalk);
+    runTestScripts(flags.dirwalk, basePath);
   } else {
     try {
       testList = require('./test-list.js').testList;
       testList.forEach(function(f) {
         if (errslast && errslast.indexOf(f) >= 0)
           return;
-        updateProgress(testScript, path.join(__dirname, 'feature', f));
+        updateProgress(testScript, f, basePath);
       });
     } catch(e) {
       if (!errslast) {