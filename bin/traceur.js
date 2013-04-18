google/traceur-compiler

commit ecafec3b59ad218c11133fec63e73bf83c4fc068

Implement 'inlineAndCompileSync'.
- Add 'loadSync' and 'loadTextFileSync' methods to InternalLoader.

Review URL: https://github.com/google/traceur-compiler/issues/250

Closes #250.

src/runtime/modules.js
@@ -55,9 +55,10 @@ var NOT_STARTED = 0;
 var LOADING = 1;
 var LOADED = 2;
 var PARSED = 3;
-var TRANSFORMED = 4;
-var COMPLETE = 5;
-var ERROR = 6;
+var DEPS_LOADED = 4;
+var TRANSFORMED = 5;
+var COMPLETE = 6;
+var ERROR = 7;
 
 /**
  * Base class representing a piece of code that is to be loaded or evaluated.
@@ -268,6 +269,7 @@ class InternalLoader {
     this.project = project;
     this.cache = new ArrayMap();
     this.urlToKey = Object.create(null);
+    this.sync_ = false;
   }
 
   get url() {
@@ -292,6 +294,18 @@ class InternalLoader {
     return xhr;
   }
 
+  loadTextFileSync(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.onerror = function(e) {
+      throw new Error(xhr.statusText);
+    };
+    xhr.open('GET', url, false);
+    xhr.send();
+    if (xhr.status == 200 || xhr.status == 0) {
+      return xhr.responseText;
+    }
+  }
+
   load(url) {
     url = resolveUrl(this.url, url);
     var codeUnit = this.getCodeUnit(url);
@@ -300,6 +314,17 @@ class InternalLoader {
     }
 
     codeUnit.state = LOADING;
+    if (this.sync_) {
+      try {
+        codeUnit.text = this.loadTextFileSync(url);
+        codeUnit.state = LOADED;
+        this.handleCodeUnitLoaded(codeUnit);
+      } catch(e) {
+        codeUnit.state = ERROR;
+        this.handleCodeUnitLoadError(codeUnit);
+      }
+      return codeUnit;
+    }
     var loader = this;
     codeUnit.xhr = this.loadTextFile(url, function(text) {
       codeUnit.text = text;
@@ -310,6 +335,13 @@ class InternalLoader {
       loader.handleCodeUnitLoadError(codeUnit);
     });
     return codeUnit;
+  }
+
+  loadSync(url) {
+    this.sync_ = true;
+    var loaded = this.load(url);
+    this.sync_ = false;
+    return loaded;
   }
 
   evalLoad(code) {
@@ -362,12 +394,17 @@ class InternalLoader {
     var requireVisitor = new ModuleRequireVisitor(this.reporter);
     requireVisitor.visit(codeUnit.tree);
     var baseUrl = codeUnit.url;
-    codeUnit.dependencies = requireVisitor.requireUrls.map((url) => {
+    var resolvedUrls = requireVisitor.requireUrls.map((url) => {
       url = resolveUrl(baseUrl, url);
+      this.getCodeUnit(url);
+      return url;
+    });
+    codeUnit.dependencies = resolvedUrls.map((url) => {
       return this.load(url);
     });
+    codeUnit.state = DEPS_LOADED;
 
-    if (this.areAll(PARSED)) {
+    if (this.areAll(DEPS_LOADED)) {
       this.analyze();
       this.transform();
       this.evaluate();

src/node/inline-module.js
@@ -146,6 +146,10 @@ InlineCodeLoader.prototype = {
       },
       abort: function() {}
     };
+  },
+
+  loadTextFileSync: function(url) {
+    return fs.readFileSync(url, 'utf8');
   }
 };
 
@@ -212,4 +216,23 @@ function inlineAndCompile(filenames, options, reporter, callback, errback) {
   loadNext();
 }
 
+function inlineAndCompileSync(filenames, options, reporter) {
+  // The caller needs to do a chdir.
+  var basePath = './';
+  var depTarget = options && options.depTarget;
+
+  var loadCount = 0;
+  var elements = [];
+  var project = new Project(basePath);
+  var loader = new InlineCodeLoader(reporter, project, elements, depTarget);
+
+  filenames.forEach(function(filename) {
+    filename = resolveUrl(basePath, filename);
+    startCodeUnit = loader.getCodeUnit(filename);
+    loader.loadSync(filename);
+  });
+  return allLoaded(basePath, reporter, elements);
+}
+
 exports.inlineAndCompile = inlineAndCompile;
+exports.inlineAndCompileSync = inlineAndCompileSync;

commit df3521dc60e95a8739fcf141be69d65ebdbcc421

Fix synchronous loading of modules
Async module loading got broken by ecafec3b59ad218c11133fec63e73bf83c4fc068.

Fixes #264

Review URL: https://github.com/google/traceur-compiler/issues/265

Closes #265, closes #264.

bin/traceur.js
@@ -19191,10 +19191,9 @@ var $___src_runtime_modules_js = (function() {
   var LOADING = 1;
   var LOADED = 2;
   var PARSED = 3;
-  var DEPS_LOADED = 4;
-  var TRANSFORMED = 5;
-  var COMPLETE = 6;
-  var ERROR = 7;
+  var TRANSFORMED = 4;
+  var COMPLETE = 5;
+  var ERROR = 6;
   var CodeUnit = function() {
     'use strict';
     var $CodeUnit = ($__createClassNoExtends)({
@@ -19444,16 +19443,14 @@ var $___src_runtime_modules_js = (function() {
         var requireVisitor = new ModuleRequireVisitor(this.reporter);
         requireVisitor.visit(codeUnit.tree);
         var baseUrl = codeUnit.url;
-        var resolvedUrls = requireVisitor.requireUrls.map((function(url) {
+        codeUnit.dependencies = requireVisitor.requireUrls.map((function(url) {
           url = resolveUrl(baseUrl, url);
-          this.getCodeUnit(url);
-          return url;
+          return this.getCodeUnit(url);
         }).bind(this));
-        codeUnit.dependencies = resolvedUrls.map((function(url) {
-          return this.load(url);
+        codeUnit.dependencies.forEach((function(dependency) {
+          this.load(dependency.url);
         }).bind(this));
-        codeUnit.state = DEPS_LOADED;
-        if (this.areAll(DEPS_LOADED)) {
+        if (this.areAll(PARSED)) {
           this.analyze();
           this.transform();
           this.evaluate();

src/runtime/modules.js
@@ -56,10 +56,9 @@ var NOT_STARTED = 0;
 var LOADING = 1;
 var LOADED = 2;
 var PARSED = 3;
-var DEPS_LOADED = 4;
-var TRANSFORMED = 5;
-var COMPLETE = 6;
-var ERROR = 7;
+var TRANSFORMED = 4;
+var COMPLETE = 5;
+var ERROR = 6;
 
 /**
  * Base class representing a piece of code that is to be loaded or evaluated.
@@ -395,17 +394,15 @@ class InternalLoader {
     var requireVisitor = new ModuleRequireVisitor(this.reporter);
     requireVisitor.visit(codeUnit.tree);
     var baseUrl = codeUnit.url;
-    var resolvedUrls = requireVisitor.requireUrls.map((url) => {
+    codeUnit.dependencies = requireVisitor.requireUrls.map((url) => {
       url = resolveUrl(baseUrl, url);
-      this.getCodeUnit(url);
-      return url;
+      return this.getCodeUnit(url);
     });
-    codeUnit.dependencies = resolvedUrls.map((url) => {
-      return this.load(url);
+    codeUnit.dependencies.forEach((dependency) => {
+      this.load(dependency.url);
     });
-    codeUnit.state = DEPS_LOADED;
 
-    if (this.areAll(DEPS_LOADED)) {
+    if (this.areAll(PARSED)) {
       this.analyze();
       this.transform();
       this.evaluate();