4442bbd54313bc8a91cfd93a28ebcb69cc523360
johnjbarton <johnjbarton@johnjbarton.com>
2013-12-19 11:47:41 -0800

    Replace WebPageProject in runner.html with new WebPageTranscoder
    Loader based testing rather than Project based.
    
    Review URL: https://github.com/google/traceur-compiler/pull/573
    
    Closes #573.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-new traceur.WebPageProject(document.location.href).run();
+new traceur.WebPageTranscoder(document.location.href).run();
