f87b08f6b4328fef0718a0e995fa4513b0efc74b
johnjbarton <johnjbarton@johnjbarton.com>
2013-12-23 17:45:09 -0800

    Rename file System.js to LoaderHooks.js
    Review URL: https://github.com/google/traceur-compiler/pull/596
    
    Closes #596.

--- src/runtime/InterceptOutputLoaderHooks.js
+++ src/runtime/InterceptOutputLoaderHooks.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {LoaderHooks} from './System';
+import {LoaderHooks} from './LoaderHooks';
 
 export class InterceptOutputLoaderHooks extends LoaderHooks {
   constructor(...args) {
