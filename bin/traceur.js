google/traceur-compiler

commit 3ca0b20a97138fdc07e3ccb539068833f8bf5aa2

Cleanup to make parse Function{Declaration,Expression} share the code.
Review URL: https://github.com/google/traceur-compiler/pull/650

Closes #650.

bin/traceur.js
@@ -11012,25 +11012,17 @@ System.registerModule("../src/syntax/Parser", function() {
       return this.parseFallThroughStatement_(allowScriptItem);
     },
     parseFunctionDeclaration_: function() {
-      var start = this.getTreeStartLocation_();
-      this.nextToken_();
-      var isGenerator = parseOptions.generators && this.eatIf_(STAR);
-      return this.parseFunctionDeclarationTail_(start, isGenerator, this.parseBindingIdentifier_());
-    },
-    parseFunctionDeclarationTail_: function(start, isGenerator, name) {
-      this.eat_(OPEN_PAREN);
-      var formalParameterList = this.parseFormalParameterList_();
-      this.eat_(CLOSE_PAREN);
-      var typeAnnotation = this.parseTypeAnnotationOpt_();
-      var functionBody = this.parseFunctionBody_(isGenerator, formalParameterList);
-      return new FunctionDeclaration(this.getTreeLocation_(start), name, isGenerator, formalParameterList, typeAnnotation, functionBody);
+      return this.parseFuntion_(FunctionDeclaration);
     },
     parseFunctionExpression_: function() {
+      return this.parseFuntion_(FunctionExpression);
+    },
+    parseFuntion_: function(ctor) {
       var start = this.getTreeStartLocation_();
-      this.nextToken_();
+      this.eat_(FUNCTION);
       var isGenerator = parseOptions.generators && this.eatIf_(STAR);
       var name = null;
-      if (this.peekBindingIdentifier_(this.peekType_())) {
+      if (ctor === FunctionDeclaration || this.peekBindingIdentifier_(this.peekType_())) {
         name = this.parseBindingIdentifier_();
       }
       this.eat_(OPEN_PAREN);
@@ -11038,7 +11030,7 @@ System.registerModule("../src/syntax/Parser", function() {
       this.eat_(CLOSE_PAREN);
       var typeAnnotation = this.parseTypeAnnotationOpt_();
       var functionBody = this.parseFunctionBody_(isGenerator, formalParameterList);
-      return new FunctionExpression(this.getTreeLocation_(start), name, isGenerator, formalParameterList, typeAnnotation, functionBody);
+      return new ctor(this.getTreeLocation_(start), name, isGenerator, formalParameterList, typeAnnotation, functionBody);
     },
     peekRest_: function(type) {
       return type === DOT_DOT_DOT && parseOptions.restParameters;

src/syntax/Parser.js
@@ -817,29 +817,7 @@ export class Parser {
    * @private
    */
   parseFunctionDeclaration_() {
-    var start = this.getTreeStartLocation_();
-    this.nextToken_(); // function or #
-    var isGenerator = parseOptions.generators && this.eatIf_(STAR);
-    return this.parseFunctionDeclarationTail_(start, isGenerator,
-                                              this.parseBindingIdentifier_());
-  }
-
-  /**
-   * @param {SourcePosition} start
-   * @param {IdentifierToken} name
-   * @return {ParseTree}
-   * @private
-   */
-  parseFunctionDeclarationTail_(start, isGenerator, name) {
-    this.eat_(OPEN_PAREN);
-    var formalParameterList = this.parseFormalParameterList_();
-    this.eat_(CLOSE_PAREN);
-    var typeAnnotation = this.parseTypeAnnotationOpt_();
-    var functionBody = this.parseFunctionBody_(isGenerator,
-                                               formalParameterList);
-    return new FunctionDeclaration(this.getTreeLocation_(start), name,
-                                   isGenerator, formalParameterList, typeAnnotation,
-                                   functionBody);
+    return this.parseFuntion_(FunctionDeclaration);
   }
 
   /**
@@ -847,22 +825,27 @@ export class Parser {
    * @private
    */
   parseFunctionExpression_() {
+    return this.parseFuntion_(FunctionExpression);
+  }
+
+  parseFuntion_(ctor) {
     var start = this.getTreeStartLocation_();
-    this.nextToken_(); // function or #
+    this.eat_(FUNCTION);
     var isGenerator = parseOptions.generators && this.eatIf_(STAR);
     var name = null;
-    if (this.peekBindingIdentifier_(this.peekType_())) {
+    if (ctor === FunctionDeclaration ||
+        this.peekBindingIdentifier_(this.peekType_())) {
       name = this.parseBindingIdentifier_();
     }
+
     this.eat_(OPEN_PAREN);
     var formalParameterList = this.parseFormalParameterList_();
     this.eat_(CLOSE_PAREN);
     var typeAnnotation = this.parseTypeAnnotationOpt_();
     var functionBody = this.parseFunctionBody_(isGenerator,
                                                formalParameterList);
-    return new FunctionExpression(this.getTreeLocation_(start), name,
-                                  isGenerator, formalParameterList, typeAnnotation,
-                                  functionBody);
+    return new ctor(this.getTreeLocation_(start), name, isGenerator,
+                    formalParameterList, typeAnnotation, functionBody);
   }
 
   peekRest_(type) {

commit 68b99354d794cc9d43f71c2d60cf5e5b95988580

Fix typo in private function name.
parserFuntion_ => parseFunction_

This typo was introduced in 3ca0b20a97138fdc07e3ccb539068833f8bf5aa2

bin/traceur.js
@@ -11012,12 +11012,12 @@ System.registerModule("../src/syntax/Parser", function() {
       return this.parseFallThroughStatement_(allowScriptItem);
     },
     parseFunctionDeclaration_: function() {
-      return this.parseFuntion_(FunctionDeclaration);
+      return this.parseFunction_(FunctionDeclaration);
     },
     parseFunctionExpression_: function() {
-      return this.parseFuntion_(FunctionExpression);
+      return this.parseFunction_(FunctionExpression);
     },
-    parseFuntion_: function(ctor) {
+    parseFunction_: function(ctor) {
       var start = this.getTreeStartLocation_();
       this.eat_(FUNCTION);
       var isGenerator = parseOptions.generators && this.eatIf_(STAR);

src/syntax/Parser.js
@@ -817,7 +817,7 @@ export class Parser {
    * @private
    */
   parseFunctionDeclaration_() {
-    return this.parseFuntion_(FunctionDeclaration);
+    return this.parseFunction_(FunctionDeclaration);
   }
 
   /**
@@ -825,10 +825,10 @@ export class Parser {
    * @private
    */
   parseFunctionExpression_() {
-    return this.parseFuntion_(FunctionExpression);
+    return this.parseFunction_(FunctionExpression);
   }
 
-  parseFuntion_(ctor) {
+  parseFunction_(ctor) {
     var start = this.getTreeStartLocation_();
     this.eat_(FUNCTION);
     var isGenerator = parseOptions.generators && this.eatIf_(STAR);