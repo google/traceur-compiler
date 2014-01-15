6d5d68a8c2823ee76415c49dcee7148b1531a5dd
Erik Arvidsson <arv@chromium.org>
2014-01-15 10:45:41 -0500

    Duplicate imports should be reported as errors
    Fixes #648
    
    Review URL: https://github.com/google/traceur-compiler/pull/667
    
    Closes #667, closes #648.

--- test/feature/Modules/Error_ExportStarConflict.js
+++ test/feature/Modules/Error_ExportStarConflict.js
@@ -1,6 +1,5 @@
 // Should not compile.
-// Error: feature/Modules/resources/export-conflict.js:2:8: Duplicate export of 'a'
-// Error: feature/Modules/resources/export-conflict.js:1:12: 'a' was previously exported here
+// Error: feature/Modules/resources/export-conflict.js:2:8: Duplicate export. 'a' was previously exported at feature/Modules/resources/export-conflict.js:1:12
 
 import {a} from './resources/export-conflict';
 
