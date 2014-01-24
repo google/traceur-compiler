fa697ea3017a4c7738053e01da0d23f12f59e214
Erik Arvidsson <arv@chromium.org>
2014-01-24 15:26:27 -0500

    Fix typo in comment

--- test/amd/ImportNoImportClause.js
+++ test/amd/ImportNoImportClause.js
@@ -1,4 +1,4 @@
-// ADM executes the dependencies before executing the module.
+// AMD executes the dependencies before executing the module.
 import './deps/side-effect';
 assert.equal(1, this.sideEffect);
 this.sideEffect = 0;
