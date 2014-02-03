132d46ca0c17abab44f95103a862f5acb077419d
Tommy Odom <odom@finelineprototyping.com>
2014-02-02 21:55:39 -0500

    Code review clean up

--- test/feature/TypeAssertions/resources/assert.js
+++ test/feature/TypeAssertions/resources/assert.js
@@ -1,7 +1,6 @@
 export var assert = this.assert;
 assert.type = function (actual, type) {
-  var typeName = type.name ? type.name :
-      type.toString().match(/^function\s*([^\s(]+)/)[1];
+  var typeName = type.name || type.toString().match(/^\s*function\s*([^\s(]+)/)[1];
   assert.typeOf(actual, typeName);
   return actual;
 };
