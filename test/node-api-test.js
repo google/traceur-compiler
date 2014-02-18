1b3d24ed14d0741a3829632248ca8fb12b9b06b5
James Lal <james@lightsofapollo.com>
2014-02-17 18:15:22 -0800

    Add module names to modules compiled with the node api

---
+++ test/node-api-test.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+suite('node public api', function() {
+  var traceur = require('../');
+  var path = __dirname + '/commonjs/BasicImport.js';
+  var contents =
+    fs.readFileSync(path, 'utf8');
+
+  test('modules: true', function() {
+    var compiled = traceur.compile(contents, {
+      // absolute path is important
+      filename: path,
+
+      // build ES6 style modules rather then cjs
+      modules: true,
+
+      // cwd is this test directory
+      cwd: __dirname,
+
+      // ensure the source map works
+      sourceMap: true
+    });
+
+    assert.deepEqual(compiled.errors, []);
+    assert.ok(compiled.js, 'can compile');
+    assert.include(
+      compiled.js,
+
+      // the module path is relative to the cwd setting when we compile it.
+      'commonjs/BasicImport',
+
+      'module defines its path'
+    );
+  });
+});
