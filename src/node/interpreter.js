4c85afb8350fa2c5c0d6916fe9d1c7223a69c47a
johnjbarton <johnjbarton@johnjbarton.com>
2014-03-02 10:29:35 -0800

    Print a stack trace when the interpreter sees an exception. Fixes #812

--- src/node/interpreter.js
+++ src/node/interpreter.js
@@ -25,7 +25,7 @@ function interpret(filename, argv, flags) {
   System.import(moduleName).then(function() {
 
   }).catch(function(err) {
-    console.error(err);
+    console.error(err.stack || err);
   });
 }
 
