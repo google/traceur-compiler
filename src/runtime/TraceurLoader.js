955eb06d9c7ee67ec1380c502d82297433cebe53
johnjbarton <johnjbarton@johnjbarton.com>
2014-04-09 07:51:18 -0700

    Impl TraceurLoader.importAll, loadAsScriptAll.
    importAll(array-of-names) promises array of modules.
    loadAsScriptAll(array-of-names) promises array of script evaluation results.
    
    Review URL: https://github.com/google/traceur-compiler/pull/949
    
    Closes #949.

--- src/runtime/TraceurLoader.js
+++ src/runtime/TraceurLoader.js
@@ -35,6 +35,17 @@ export class TraceurLoader extends Loader {
   }
 
   /**
+    * @param {Array<string>} module names
+    * @param {Object} referrerName and address passed to normalize.
+    * @return {Promise} fulfilled with array of evaluated modules
+    */
+  importAll(names, {referrerName, address} = {}) {
+    return Promise.all(names.map((name) => {
+      return this.import(name, {referrerName: referrerName, address: address});
+    }));
+  }
+
+  /**
    * See https://github.com/jorendorff/js-loaders/issues/92
    * loadAsScript - Asynchronously load and run a script. If the script
    * calls Loader.import(),  this can cause modules to be loaded, linked,
@@ -50,6 +61,13 @@ export class TraceurLoader extends Loader {
   loadAsScript(name, {referrerName, address} = {}) {
     return this.internalLoader_.load(name, referrerName, address, 'script').
         then((codeUnit) => codeUnit.result);
+  }
+
+  loadAsScriptAll(names, {referrerName, address} = {}) {
+    return Promise.all(names.map((name) => {
+      return this.loadAsScript(name,
+          {referrerName: referrerName, address: address});
+    }));
   }
 
   /**
