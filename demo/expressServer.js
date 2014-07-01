f1c48f08487894cf3dc5a1698e30b4257b0b95b0
johnjbarton <johnjbarton@johnjbarton.com>
2014-06-30 17:41:33 -0700

    Upgrade to express 4.x and add devDependency

--- demo/expressServer.js
+++ demo/expressServer.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var http = require('http');
+var serveIndex = require('serve-index');
 
 function servePathAtPort(path, port) {
   var app = express();
   app.use(express.static(path));   // before directory to allow index.html to work
-  app.use(express.directory(path));
+  app.use(serveIndex(path));
   var server = http.createServer(app);
   server.on('error', function(e) {
   	console.log('Port ' + port + ' did not work out');
