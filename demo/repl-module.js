google/traceur-compiler

commit 75ca1e0b25027bb152b96245a899bd7d4b57178a

Insert repl options at top of file based on user's choices.
Also rely less on global options.
Fixes #1213.

Review URL: https://api.github.com/repos/google/traceur-compiler/issues/1219

Closes #1219, closes #1213.

demo/repl-module.js
@@ -21,9 +21,7 @@ import {SourceMapConsumer}
     from 'traceur@0.0/src/outputgeneration/SourceMapIntegration';
 import {transcode, renderSourceMap} from './transcode';
 import {options as traceurOptions} from 'traceur@0.0/src/Options';
-import {
-  setOptionsFromSource
-} from './replOptions';
+import {setOptionsFromSource} from './replOptions';
 
 var hasError = false;
 var debouncedCompile = debounced(compile, 200, 2000);
@@ -154,15 +152,30 @@ function compile() {
 
   var name = 'repl';
   var contents = input.getValue();
+  traceurOptions.setFromObject(
+    setOptionsFromSource(contents, resetAndCompileContents));
+
+  compileContents(contents);
+}
+
+// When options are changed we write // Options back into the source
+// and recompile with these options.
+function resetAndCompileContents(contents, newOptions) {
+  input.setValue(contents);
+
   if (history.replaceState)
     history.replaceState(null, document.title,
                          '#' + encodeURIComponent(contents));
 
-  setOptionsFromSource(contents, compile);
+  traceurOptions.setFromObject(newOptions);
+  compileContents(contents);
+}
+
+function compileContents(contents) {
 
   errorElement.hidden = true;
   function onSuccess(mod) {
-    // Empty for now.
+    console.log('Module evaluation succeeded');
   }
   function onFailure(error) {
      hasError = true;

demo/repl.html
@@ -225,7 +225,9 @@ h1 {
 </div>
 <div id='mocha'></div>
 <script>
-System.import('./repl-module');
+System.import('./repl-module').catch(function(ex) {
+  console.error('Internal Error ', ex.stack || ex);
+});
 </script>
 </body>
 </html>

demo/replOptions.js
@@ -12,32 +12,81 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {options as traceurOptions} from 'traceur@0.0/src/Options';
+import {
+  Options,
+  CommandOptions,
+  versionLockedOptions,
+  toDashCase
+} from 'traceur@0.0/src/Options';
 
-// Show correlation of input and generated source by default
-traceurOptions.sourceMaps = true;
 
 var optionChangeHandler;
+var traceurOptions;
+var showAllOpts = false;
+var allOptsLength;
+var showMax;
+var firstPass = true;
+var reOptions = /^\/\/ Options:\s*(.+)$/mg;
 
 export function setOptionsFromSource(source, onOptionChanged) {
-  var re = /^\/\/ Options:\s*(.+)$/mg;
-  var optionLines = source.match(re);
+
+  // Start with default options.
+  traceurOptions = new Options();
+
+  // Mutate these options with source-defined // Options values.
+  var optionLines = source.match(reOptions);
   if (optionLines) {
     optionLines.forEach(function(line) {
-      re.lastIndex = 0;
-      var m = re.exec(line);
+      reOptions.lastIndex = 0;
+      var m = reOptions.exec(line);
       try {
-        traceurOptions.fromString(m[1]);
+        traceurOptions.setFromObject(CommandOptions.fromString(m[1]));
+        if (firstPass) {
+          // Show correlation of input and generated source by default
+          traceurOptions.sourceMaps = true;
+          firstPass = false;
+        }
       } catch (ex) {
-        // Ignore unknown options.
+        console.error('Internal Error: ', ex.stack || ex);
       }
     });
-    createOptions();
   }
-  optionChangeHandler = onOptionChanged;
+  createMatchingOptionControls(traceurOptions);
+  optionChangeHandler = (newOptions) => {
+    source = prependSourceWithNewOptions(newOptions, source);
+    onOptionChanged(source, newOptions);
+  };
+
+  allOptsLength = Object.keys(traceurOptions).length;
+  showMax = allOptsLength;
+
+  return traceurOptions;
 }
 
-function createOptionRow(name) {
+function optionsToSource(newOptions) {
+  var line = '';
+  for (var key in versionLockedOptions) {
+    if (versionLockedOptions[key] !== newOptions[key]) {
+      line += '--' + toDashCase(key);
+      if (!newOptions[key])
+        line += '=' + newOptions[key];
+      line += ' ';
+    }
+  }
+  return line && '// Options: ' + line + '\n';
+}
+
+function prependSourceWithNewOptions(newOptions, source) {
+  source = source.split('\n').reduce((linesNoOptions, line) => {
+    reOptions.lastIndex = 0;
+    if (!reOptions.test(line))
+      linesNoOptions.push(line);
+    return linesNoOptions;
+  }, []).join('\n');
+  return optionsToSource(newOptions) + source;
+}
+
+function createOptionRow(traceurOptions, name) {
   var label = document.createElement('label');
   label.textContent = name;
   var cb = label.insertBefore(document.createElement('input'),
@@ -48,8 +97,7 @@ function createOptionRow(name) {
   cb.indeterminate = checked === null;
   cb.onclick = function() {
     traceurOptions[name] = cb.checked;
-    createOptions();
-    optionChangeHandler();
+    optionChangeHandler(traceurOptions);
   };
   return label;
 }
@@ -62,11 +110,7 @@ var extraOptions = [
   'validate'
 ];
 
-var showAllOpts = false;
-var allOptsLength = Object.keys(traceurOptions).length;
-var showMax = allOptsLength;
-
-function createOptions() {
+function createMatchingOptionControls(traceurOptions) {
   var optionsDiv = document.querySelector('.traceur-options');
   optionsDiv.textContent = '';
   if (showAllOpts) {
@@ -74,39 +118,37 @@ function createOptions() {
     Object.keys(traceurOptions).forEach(function(name) {
       if (i++ >= showMax || extraOptions.lastIndexOf(name) >= 0)
         return;
-      optionsDiv.appendChild(createOptionRow(name));
+      optionsDiv.appendChild(createOptionRow(traceurOptions, name));
     });
     optionsDiv.appendChild(document.createElement('hr'));
   }
   extraOptions.forEach(function(name) {
-    optionsDiv.appendChild(createOptionRow(name));
+    optionsDiv.appendChild(createOptionRow(traceurOptions, name));
   });
 }
 
-createOptions();
-
-function rebuildOptions() {
+function rebuildOptions(traceurOptions) {
   var optionsDiv = document.querySelector('.traceur-options');
   optionsDiv.innerHTML = '';
-  createOptions();
+  optionChangeHandler(traceurOptions);
 }
 
 document.querySelector('.reset-all').addEventListener('click',
     function() {
       traceurOptions.reset();
-      rebuildOptions();
+      rebuildOptions(traceurOptions);
     });
 
 document.querySelector('.all-off').addEventListener('click',
     function() {
       traceurOptions.reset(true);
-      rebuildOptions();
+      rebuildOptions(traceurOptions);
     });
 
 document.querySelector('.show-all-toggle').addEventListener('click',
     function() {
       showAllOpts = !showAllOpts;
-      rebuildOptions();
+      rebuildOptions(traceurOptions);
     });
 
 document.querySelector('.option-button').addEventListener('click',
@@ -114,41 +156,3 @@ document.querySelector('.option-button').addEventListener('click',
       var optionsDiv = document.querySelector('.options');
       optionsDiv.hidden = !optionsDiv.hidden;
     });
-
-var codeCur = 0;
-var code = 'UUDDLRLRBA'.split('').map(function(k) {
-  return {'U': 38, 'D': 40, 'L': 37, 'R': 39, 'A': 65, 'B': 66}[k];
-});
-
-document.addEventListener('keyup', function(e) {
-  if (e.keyCode !== code[codeCur++])
-    codeCur = +(e.keyCode === code[0]);
-  if (codeCur === code.length) {
-    var optionsDiv = document.querySelector('.options');
-    optionsDiv.hidden = false;
-    optionsDiv.classList.add('god0');
-    window.setInterval(updateTransition, 500);
-    showAllOpts = !showAllOpts;
-    showMax = 0;
-    rebuildAnimate();
-  }
-})
-
-function rebuildAnimate() {
-  if (showMax++ >= allOptsLength)
-    return;
-  rebuildOptions();
-  var optionsDiv = document.querySelector('.options');
-  setTimeout(rebuildAnimate, 1000);
-}
-
-function updateTransition() {
-  var optionsDiv;
-  for (var i = 0; i < 2; i++) {
-    if (optionsDiv = document.querySelector('.god' + i)) {
-      optionsDiv.classList.remove('god' + i);
-      optionsDiv.classList.add('god' + (i + 1) % 2);
-      break;
-    }
-  }
-}

src/Options.js
@@ -323,7 +323,7 @@ function toCamelCase(s) {
 /**
  * Converts a string from aaaBbbCcc to aaa-bbb-ccc.
  */
-function toDashCase(s) {
+export function toDashCase(s) {
   return s.replace(/[A-Z]/g, function(ch) {
     return '-' + ch.toLowerCase();
   });

commit 5d46db77d8bc46f2dbf248e579fee75560f61715

Repl: Fix issue where location  was not updated.
This was a regression from 75ca1e0b25027bb152b96245a899bd7d4b57178a

Fixes #1234

demo/repl-module.js
@@ -146,12 +146,20 @@ function showRange(range) {
 
 var compilationResults = {};
 
+function updateLocation(contents) {
+  if (history.replaceState) {
+    history.replaceState(null, document.title,
+                         '#' + encodeURIComponent(contents));
+  }
+}
+
 function compile() {
   hasError = false;
   output.setValue('');
 
   var name = 'repl';
   var contents = input.getValue();
+  updateLocation(contents);
   try {
     traceurOptions.setFromObject(
         setOptionsFromSource(contents, resetAndCompileContents));
@@ -169,11 +177,7 @@ function compile() {
 // and recompile with these options.
 function resetAndCompileContents(contents, newOptions) {
   input.setValue(contents);
-
-  if (history.replaceState)
-    history.replaceState(null, document.title,
-                         '#' + encodeURIComponent(contents));
-
+  updateLocation(contents);
   traceurOptions.setFromObject(newOptions);
   compileContents(contents);
 }