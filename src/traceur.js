53692146d8ff372dc91f8ffb0ba8102c60bd7abb
guybedford <guybedford@gmail.com>
2014-08-03 22:16:29 -0700

    expose traceur.get

--- src/traceur.js
+++ src/traceur.js
@@ -20,6 +20,12 @@ export {WebPageTranscoder} from './WebPageTranscoder';
 export {options} from './Options';
 import {addOptions, CommandOptions} from './Options';
 
+import {ModuleStore} from '@traceur/src/runtime/ModuleStore';
+
+export function get(name) {
+  return ModuleStore.get(ModuleStore.normalize('./' + name, __moduleName));
+}
+
 export {Compiler} from './Compiler';
 
 import {ErrorReporter} from './util/ErrorReporter';
