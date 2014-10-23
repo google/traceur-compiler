9ea78c9624c5d298e977eef0b14d94799e23a7ee
johnjbarton <johnjbarton@johnjbarton.com>
2014-10-23 09:17:17 -0700

    Change test to fail showing require() failure. Here we require() from an import-ed es6 module in a different directory. The require() call in the app fails to consider the different path of the call site.

--- test/unit/node/resources/requireAndImport.js
+++
@@ -1,8 +0,0 @@
-import {x} from './reexport-x';
-var path = require('path');
-var aNodeModule = require('./aNodeModule.js');
-
-if (path && path.resolve && aNodeModule.aNodeExport) {
-	var nodeExport = aNodeModule.aNodeExport;
-  console.log(`we have path and x=${x} and aNodeExport=${nodeExport}`);
-}
