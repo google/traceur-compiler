19512031ddfbd13ab6c21861ed41bda91b087992
Oliver Joseph Ash <oliverjash@icloud.com>
2014-11-18 23:12:59 +0100

    Implement `Promise.all(iterable)`

--- src/runtime/polyfills/Promise.js
+++ src/runtime/polyfills/Promise.js
@@ -150,18 +150,27 @@ export class Promise {
     var deferred = getDeferred(this);
     var resolutions = [];
     try {
-      var count = values.length;
+      var count = 0;
+      var i = 0;
+      for (var value of values) {
+        var countdownFunction = makeCountdownFunction(i);
+        this.resolve(value).then(
+            countdownFunction,
+            (r) => { deferred.reject(r); });
+        ++i
+        ++count;
+      }
+      // iterable must be empty as otherwise the count wouldn't be decreased
+      // until next tick at least
       if (count === 0) {
         deferred.resolve(resolutions);
-      } else {
-        for (var i = 0; i < values.length; i++) {
-          this.resolve(values[i]).then(
-              function(i, x) {
-                resolutions[i] = x;
-                if (--count === 0)
-                  deferred.resolve(resolutions);
-              }.bind(undefined, i),
-              (r) => { deferred.reject(r); });
+      }
+
+      function makeCountdownFunction(i) {
+        return (x) => {
+          resolutions[i] = x;
+          if (--count === 0)
+            deferred.resolve(resolutions);
         }
       }
     } catch (e) {
