76f70357002f02ae3236ef52eb1d6e3f839c97ba
Valeriy <valeriy.sorokobatko@gmail.com>
2014-12-03 01:11:48 +0200

    added Promsie to compileAllJsFilesInDir

--- src/node/compileAllJsFilesInDir.js
+++ src/node/compileAllJsFilesInDir.js
@@ -21,15 +21,27 @@ var NodeCompiler = require('./NodeCompiler.js').NodeCompiler;
 function compileAllJsFilesInDir(inputDir, outputDir, options) {
   inputDir = path.normalize(inputDir).replace(/\\/g, '/');
   outputDir = path.normalize(outputDir).replace(/\\/g, '/');
-  glob(inputDir + '/**/*.js', {}, function (er, files) {
-    if (er)
-      throw new Error('While scanning ' + inputDir + ': ' + er);
+  return new Promise(function(resolve, reject) {
+    glob(inputDir + '/**/*.js', {}, function (er, files) {
+      if (er)
+        throw new Error('While scanning ' + inputDir + ': ' + er);
 
-    files.forEach(function(inputFilePath) {
-      var outputFilePath = inputFilePath.replace(inputDir, outputDir);
-      var compiler = new NodeCompiler(options);
-      compiler.compileSingleFile(inputFilePath, outputFilePath, function(err) {
-        throw new Error('While reading ' + inputFilePath + ': ' + err);
+      var total = files.length,
+        current = 1;
+
+
+      files.forEach(function (inputFilePath) {
+        var outputFilePath = inputFilePath.replace(inputDir, outputDir);
+        var compiler = new NodeCompiler(options);
+        compiler.compileSingleFile(inputFilePath, outputFilePath, function (err) {
+          throw new Error('While reading ' + inputFilePath + ': ' + err);
+        });
+
+        if (current == total){
+          resolve();
+        } else {
+          current++;
+        }
       });
     });
   });
