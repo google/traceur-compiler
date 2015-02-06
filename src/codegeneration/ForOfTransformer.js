98f569912e8ccd0b21c7496b21004c18465e7dbb
Marc Nieper-Wißkirchen <marc@nieper-wisskirchen.de>
2015-02-06 11:56:55 +0100

    Fix #1677.
    Test test/feature/Yield/ForOfIteratorException.js passes now.

--- src/codegeneration/ForOfTransformer.js
+++ src/codegeneration/ForOfTransformer.js
@@ -12,14 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {VARIABLE_DECLARATION_LIST} from '../syntax/trees/ParseTreeType.js';
+import {
+  FOR_OF_STATEMENT,
+  VARIABLE_DECLARATION_LIST,
+  LABELLED_STATEMENT
+} from '../syntax/trees/ParseTreeType.js';
 import {TempVarTransformer} from './TempVarTransformer.js';
 import {
   createIdentifierExpression as id,
   createMemberExpression,
   createVariableStatement
 } from './ParseTreeFactory.js';
-import {parseStatement} from './PlaceholderParser.js';
+import {
+  parseStatement,
+  parseStatements
+} from './PlaceholderParser.js';
+import {
+  AnonBlock,
+  LabelledStatement
+} from '../syntax/trees/ParseTrees.js';
 
 /**
  * Desugars for-of statement.
@@ -27,12 +38,24 @@ import {parseStatement} from './PlaceholderParser.js';
 export class ForOfTransformer extends TempVarTransformer {
   /**
    * @param {ForOfStatement} original
+   * @param {Array.<LabelledStatement>=} labelSet
    * @return {ParseTree}
    */
   transformForOfStatement(original) {
+    return this.transformForOfStatement_(original, []);
+  }
+
+  transformForOfStatement_(original, labelSet) {
     var tree = super.transformForOfStatement(original);
     var iter = id(this.getTempIdentifier());
     var result = id(this.getTempIdentifier());
+    var label = id(this.getTempIdentifier());
+    var normalCompletion = id(this.getTempIdentifier());
+    var throwCompletion = id(this.getTempIdentifier());
+    var exception = id(this.getTempIdentifier());
+    var ex = id(this.getTempIdentifier());
+    var labelledStatement;
+    var innerStatement;
 
     var assignment;
     if (tree.initializer.type === VARIABLE_DECLARATION_LIST) {
@@ -45,14 +68,54 @@ export class ForOfTransformer extends TempVarTransformer {
       assignment = parseStatement `${tree.initializer} = ${result}.value;`;
     }
 
-    return parseStatement `
-        for (var ${iter} =
-                 ${tree.collection}[
-                     $traceurRuntime.toProperty(Symbol.iterator)](),
-                 ${result};
-             !(${result} = ${iter}.next()).done; ) {
-          ${assignment};
-          ${tree.body};
+    innerStatement = parseStatement `
+        for (var ${result},
+                 ${iter} = ${tree.collection}[
+                     $traceurRuntime.toProperty(Symbol.iterator)]();
+             !(${normalCompletion} = (${result} = ${iter}.next()).done);
+             ${normalCompletion} = true) {
+          ${assignment}
+          ${tree.body}
         }`;
+  
+    while (labelledStatement = labelSet.pop()) {
+      innerStatement = new LabelledStatement(labelledStatement.location,
+          labelledStatement.name, innerStatement);
+    }
+
+    return new AnonBlock(null, parseStatements `
+        var ${normalCompletion} = true;
+        var ${throwCompletion} = false;
+        var ${exception} = undefined;
+        try {
+          ${innerStatement}
+        } catch (${ex}) {
+          ${throwCompletion} = true;
+          ${exception} = ${ex};
+        } finally {
+          try {
+            if (!${normalCompletion} && ${iter}.return != null) {
+              ${iter}.return();
+            }
+          } finally {
+            if (${throwCompletion}) {
+              throw ${exception};
+            }
+          }
+        }`);
+  }
+
+  transformLabelledStatement(tree) {
+    var labelSet = [tree];
+    var statement = tree.statement;
+    while (statement.type === LABELLED_STATEMENT) {
+      labelSet.push(statement);
+      statement = statement.statement;
+    }
+    if (statement.type !== FOR_OF_STATEMENT) {
+      return super.transformLabelledStatement(tree);
+      // Slightly inefficient in the (unlikely) case of more than one label
+    }
+    return this.transformForOfStatement_(statement, labelSet);
   }
 }
