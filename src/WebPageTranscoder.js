a7810e5c5f1fd7d22c80adf636b9a174beff2761
johnjbarton <johnjbarton@johnjbarton.com>
2015-04-23 17:17:29 -0700

    Import Options needed for default in WebPageTranscoder. Fixup browser demos

--- src/WebPageTranscoder.js
+++ src/WebPageTranscoder.js
@@ -17,6 +17,7 @@
 import {Loader} from './runtime/Loader.js';
 import {BrowserTraceurLoader} from './runtime/TraceurLoader.js';
 import {ErrorReporter} from './util/ErrorReporter.js';
+import {Options} from './Options.js';
 
 export const scriptSelector = 'script[type="module"],script[type="text/traceur"]';
 
